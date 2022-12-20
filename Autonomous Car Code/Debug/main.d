c06df8ef9b22a9888c26ee80321216d4f3f853d2
Mohamed Ashraf <96884905+Gello1@users.noreply.github.com>
2022-12-21 01:05:57 +0200

    Add files via upload

---
+++ Autonomous Car Code/Debug/main.d
@@ -0,0 +1,122 @@
+main.o main.o: ../main.c ../LIB/STD_TYPES.H ../LIB/Bit_Math.h \
+  ../MCAL/MDIO/MDIO_Interface.h ../MCAL/MDIO/../../LIB/STD_TYPES.H \
+  ../HAL/HCLCD/HCLCD_Interface.h \
+  ../HAL/HCLCD/../../MCAL/MDIO/MDIO_Interface.h \
+  ../HAL/HKPD/HKPD_Interface.h ../HAL/HKPD/../../LIB/STD_TYPES.H \
+  ../HAL/HKPD/../../LIB/Bit_Math.h \
+  ../HAL/HKPD/../../MCAL/MDIO/MDIO_Interface.h \
+  ../HAL/HKPD/../HCLCD/HCLCD_Config.h \
+  ../HAL/HKPD/../HCLCD/HCLCD_Private.h \
+  ../HAL/HKPD/../HCLCD/HCLCD_Private.h \
+  ../HAL/HKPD/../HCLCD/HCLCD_Interface.h \
+  ../HAL/HSEVEN_SEG/HSEVEN_SEG_Interface.h \
+  ../HAL/HSEVEN_SEG/../../LIB/Bit_Math.h \
+  ../HAL/HSEVEN_SEG/../../LIB/STD_TYPES.H ../HAL/HUS/HUS_Interface.h \
+  ../HAL/HUS/../../LIB/Bit_Math.h ../HAL/HUS/../../LIB/STD_TYPES.H \
+  ../HAL/HUS/../../MCAL/MDIO/MDIO_Interface.h \
+  ../HAL/HUS/../../MCAL/MTIMER/MTIMER_Interface.h \
+  ../HAL/HUS/../../MCAL/MTIMER/../../LIB/Bit_Math.h \
+  ../HAL/HUS/../../MCAL/MTIMER/../../LIB/STD_TYPES.H \
+  ../HAL/HUS/../../MCAL/MTIMER/../MDIO/MDIO_Interface.h \
+  ../HAL/HUS/../../MCAL/MTIMER/MTIMER_Config.h \
+  ../HAL/HUS/../../MCAL/MTIMER/MTIMER_Private.h \
+  ../HAL/HUS/../../MCAL/MTIMER/MTIMER_Config.h \
+  ../HAL/HUS/../../MCAL/MEXTI/MEXTI_Interface.h \
+  ../HAL/HUS/../../MCAL/MEXTI/../../LIB/Bit_Math.h \
+  ../HAL/HUS/../../MCAL/MEXTI/../../LIB/STD_TYPES.H \
+  ../HAL/HUS/../../MCAL/MEXTI/../MDIO/MDIO_Interface.h \
+  ../HAL/HUS/HUS_Private.h ../HAL/HDCM/HDCM_Interface.h \
+  ../HAL/HDCM/../../LIB/Bit_Math.h ../HAL/HDCM/../../LIB/STD_TYPES.H \
+  ../HAL/HDCM/../../MCAL/MDIO/MDIO_Interface.h ../HAL/HDCM/HDCM_Private.h \
+  ../MCAL/MEXTI/MEXTI_Interface.h ../MCAL/MGIE/MGIE_Interface.h \
+  ../HAL/HSERVO/HSERVO_Interface.h \
+  ../HAL/HSERVO/../../MCAL/MDIO/MDIO_Interface.h \
+  ../HAL/HSERVO/../../LIB/Bit_Math.h ../HAL/HSERVO/../../LIB/STD_TYPES.H
+
+../LIB/STD_TYPES.H:
+
+../LIB/Bit_Math.h:
+
+../MCAL/MDIO/MDIO_Interface.h:
+
+../MCAL/MDIO/../../LIB/STD_TYPES.H:
+
+../HAL/HCLCD/HCLCD_Interface.h:
+
+../HAL/HCLCD/../../MCAL/MDIO/MDIO_Interface.h:
+
+../HAL/HKPD/HKPD_Interface.h:
+
+../HAL/HKPD/../../LIB/STD_TYPES.H:
+
+../HAL/HKPD/../../LIB/Bit_Math.h:
+
+../HAL/HKPD/../../MCAL/MDIO/MDIO_Interface.h:
+
+../HAL/HKPD/../HCLCD/HCLCD_Config.h:
+
+../HAL/HKPD/../HCLCD/HCLCD_Private.h:
+
+../HAL/HKPD/../HCLCD/HCLCD_Private.h:
+
+../HAL/HKPD/../HCLCD/HCLCD_Interface.h:
+
+../HAL/HSEVEN_SEG/HSEVEN_SEG_Interface.h:
+
+../HAL/HSEVEN_SEG/../../LIB/Bit_Math.h:
+
+../HAL/HSEVEN_SEG/../../LIB/STD_TYPES.H:
+
+../HAL/HUS/HUS_Interface.h:
+
+../HAL/HUS/../../LIB/Bit_Math.h:
+
+../HAL/HUS/../../LIB/STD_TYPES.H:
+
+../HAL/HUS/../../MCAL/MDIO/MDIO_Interface.h:
+
+../HAL/HUS/../../MCAL/MTIMER/MTIMER_Interface.h:
+
+../HAL/HUS/../../MCAL/MTIMER/../../LIB/Bit_Math.h:
+
+../HAL/HUS/../../MCAL/MTIMER/../../LIB/STD_TYPES.H:
+
+../HAL/HUS/../../MCAL/MTIMER/../MDIO/MDIO_Interface.h:
+
+../HAL/HUS/../../MCAL/MTIMER/MTIMER_Config.h:
+
+../HAL/HUS/../../MCAL/MTIMER/MTIMER_Private.h:
+
+../HAL/HUS/../../MCAL/MTIMER/MTIMER_Config.h:
+
+../HAL/HUS/../../MCAL/MEXTI/MEXTI_Interface.h:
+
+../HAL/HUS/../../MCAL/MEXTI/../../LIB/Bit_Math.h:
+
+../HAL/HUS/../../MCAL/MEXTI/../../LIB/STD_TYPES.H:
+
+../HAL/HUS/../../MCAL/MEXTI/../MDIO/MDIO_Interface.h:
+
+../HAL/HUS/HUS_Private.h:
+
+../HAL/HDCM/HDCM_Interface.h:
+
+../HAL/HDCM/../../LIB/Bit_Math.h:
+
+../HAL/HDCM/../../LIB/STD_TYPES.H:
+
+../HAL/HDCM/../../MCAL/MDIO/MDIO_Interface.h:
+
+../HAL/HDCM/HDCM_Private.h:
+
+../MCAL/MEXTI/MEXTI_Interface.h:
+
+../MCAL/MGIE/MGIE_Interface.h:
+
+../HAL/HSERVO/HSERVO_Interface.h:
+
+../HAL/HSERVO/../../MCAL/MDIO/MDIO_Interface.h:
+
+../HAL/HSERVO/../../LIB/Bit_Math.h:
+
+../HAL/HSERVO/../../LIB/STD_TYPES.H:
